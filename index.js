aa81f2e0bfe204337a83d13ee404147858b725b4
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-06-29 17:18:35 +0530

    cross button

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ input.addEventListener('keypress', (event)=>{
     if(event.key==="Enter"){
         console.log(input.value)
         renderData(input.value)
+        input.value=""
     }
 })
 
@@ -13,14 +14,20 @@ let renderData = (data)=>{
     let text = document.createElement("span")
     let circle = document.createElement("div")
     let cross = document.createElement("img")
-    let line = document.createElement("hr")
+    let line = document.createElement("hr") 
+    
 
     text.textContent=data;
     text.classList.add("txt")
     circle.classList.add("circle")
     taskBox.appendChild(circle)
     taskBox.appendChild(text)
+    cross.src="./images/icon-cross.svg"
+    cross.classList.add("cross")
+    cross.classList.add("flex-end")
+    taskBox.appendChild(cross)
     taskBox.classList.add("flex")
+    // taskBox.classList.add("space-between")
     taskBox.classList.add("txt-box")
     tasksContainer.appendChild(taskBox)
     tasksContainer.append(line)
